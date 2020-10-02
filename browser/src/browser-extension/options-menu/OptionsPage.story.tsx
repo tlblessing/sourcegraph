59385ce7b37d121f6297a9118824da1b69927121
Marek <marekz@gmail.com>
2020-10-02 12:15:38 -0400

    Update and add to storybook

--- browser/src/browser-extension/options-menu/OptionsPage.story.tsx
+++ browser/src/browser-extension/options-menu/OptionsPage.story.tsx
@@ -1,7 +1,13 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import { storiesOf } from '@storybook/react'
 import { OptionsPage } from './OptionsPage'
 import optionsStyles from '../../options.scss'
+import { Observable, of } from 'rxjs'
+import { action } from '@storybook/addon-actions'
+import { boolean, text } from '@storybook/addon-knobs'
+
+const validateSourcegraphUrl = (url: string): Observable<string | undefined> => of(undefined)
+const invalidSourcegraphUrl = (url: string): Observable<string | undefined> => of('Arbitrary error string')
 
 storiesOf('browser/Options/OptionsPage', module)
     .addDecorator(story => (
@@ -10,4 +16,39 @@ storiesOf('browser/Options/OptionsPage', module)
             <div>{story()}</div>
         </>
     ))
-    .add('Default', () => <OptionsPage version="0.0.0" />)
+    .add('Default', () => (
+        <OptionsPage
+            version={text('version', '0.0.0')}
+            isCurrentRepositoryPrivate={boolean('isCurrentRepositoryPrivate', false)}
+            validateSourcegraphUrl={validateSourcegraphUrl}
+            onToggleActivated={action('onToggleActivated')}
+            isActivated={true}
+            sourcegraphUrl={text('sourcegraphUrl', 'https://sourcegraph.com')}
+        />
+    ))
+    .add('Interactive', () => {
+        const [isActivated, setIsActivated] = useState(false)
+        return (
+            <OptionsPage
+                version={text('version', '0.0.0')}
+                isActivated={isActivated}
+                onToggleActivated={setIsActivated}
+                validateSourcegraphUrl={validateSourcegraphUrl}
+                sourcegraphUrl={text('sourcegraphUrl', 'https://sourcegraph.com')}
+                isCurrentRepositoryPrivate={boolean('isCurrentRepositoryPrivate', false)}
+            />
+        )
+    })
+    .add('URL validation error', () => {
+        const [isActivated, setIsActivated] = useState(false)
+        return (
+            <OptionsPage
+                version={text('version', '0.0.0')}
+                isActivated={isActivated}
+                onToggleActivated={setIsActivated}
+                validateSourcegraphUrl={invalidSourcegraphUrl}
+                sourcegraphUrl={text('sourcegraphUrl', 'https://not-sourcegraph.com')}
+                isCurrentRepositoryPrivate={boolean('isCurrentRepositoryPrivate', false)}
+            />
+        )
+    })
